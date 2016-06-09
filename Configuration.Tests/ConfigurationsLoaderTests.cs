a215504cdbd35db007fa8da9e6bb627335471804
Ali Gilgil <ali.gilgil1@gmail.com>
2016-06-09 18:24:42 -0400

    errors during merge

---
+++ Configuration.Tests/ConfigurationsLoaderTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Configuration.Tests
+{
+    [TestClass]
+    public class ConfigurationsLoaderTests
+    {
+        private readonly string _expectedUri = "https://tdd.reddit.com/api/";
+
+        [TestMethod]
+        public async Task GivenValidFile_Load_ValidConfigurationReturned()
+        {
+            const string expectedConfigurationJsonified = "{\"redditApiEndpoint\": \"https://tdd.reddit.com/api/\"}";
+            var mockedStreamReader = new Mock<TextReader>();
+            mockedStreamReader.Setup(s => s.ReadToEndAsync()).ReturnsAsync(expectedConfigurationJsonified);
+
+            var configurationsLoader = new ConfigurationsLoader();
+            Configurations configurations = await configurationsLoader.LoadAsync(mockedStreamReader.Object);
+
+            Assert.AreEqual(_expectedUri, configurations.RedditApiEndpoint);
+        }
+
+        [TestMethod, ExpectedException(typeof(ConfigurationsLoadingException))]
+        public async Task GivenNotExistingFile_Load_ConfigurationsLoadingExeptionThrown()
+        {
+            var mockedStreamReader = new Mock<TextReader>();
+            mockedStreamReader.Setup(s => s.ReadToEndAsync()).ThrowsAsync(new FileNotFoundException("File not found."));
+
+            var configurationsLoader = new ConfigurationsLoader();
+            Configurations configurations = await configurationsLoader.LoadAsync(mockedStreamReader.Object);
+
+            Assert.AreEqual(_expectedUri, configurations.RedditApiEndpoint);
+        }
+    }
+}
---
+++ OhShitWaddup/Program.cs
@@ -0,0 +1,51 @@
+﻿using Configuration;
+using Listener;
+using Reddit;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OhShitWaddup
+{
+    class Program
+    {
+        private static Lazy<HttpClient> _httpClient = new Lazy<HttpClient>(() => new HttpClient(), true);
+        private static string _configurationFileName = "configurationFile.json";
+
+        static void Main(string[] args)
+        {
+            Task.Run(() => Run());
+        }
+
+        private async static Task Run()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                TaskedListener taskedListener = await CreateListenerAsync();
+
+                await taskedListener.Start(new CancellationToken());
+
+                Console.Read();
+            }
+        }
+
+        private async static Task<TaskedListener> CreateListenerAsync()
+        {
+            Configurations configurations = await LoadConfigurationsAsync();
+
+            CommentsRepository commentsRepository = new CommentsRepository(_httpClient.Value, new Uri(configurations.RedditApiEndpoint));
+            RedditListener redditListener = new RedditListener(commentsRepository);
+
+            return new TaskedListener(redditListener);
+        }
+
+        private async static Task<Configurations> LoadConfigurationsAsync()
+        {
+            ConfigurationsLoader configurationsLoader = new ConfigurationsLoader();
+
+            return await configurationsLoader.LoadAsync(new StreamReader(_configurationFileName));
+        }
+    }
+}
